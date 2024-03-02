9d330fa68b25244c799ec7746cb63b3859f3786a
sheteshreyash <shreyashshete21@gmail.com>
2024-03-02 20:18:29 +0530

    added landing page code and demo signup page code

---
+++ IE NGENEX/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import '../src/style/App.css';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Products from './pages/Products';
+import SignUp from './pages/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PreLoader from './components/Preloader';
+import './style/components/Preloader.css'
+import { CookieConsent } from 'react-cookie-consent';
+
+function App() {
+  return (
+    <>
+      <PreLoader />
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+
+
+      <CookieConsent
+        debug={true}
+        location='bottom'
+        style={{ background: 'black' }}
+        buttonStyle={{ color: 'black', background: 'white', fontSize: '14px' }}
+        buttonText = 'Got it!'
+      >
+        This Site uses Cookies. Visit our <a href="/privacy">privacy policy</a> for more.
+      </CookieConsent>
+    </>
+  );
+}
+
+export default App;
---
+++ IE NGENEX/src/pages/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../style/App.css';
+
+export default function SignUp() {
+  return <div className='sign-up'>
+    LIKE & SUBSCRIBE
+  </div>;
+}
