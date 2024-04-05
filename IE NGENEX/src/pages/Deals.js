27729618d90261b4198b80c045303924f95a83b2
sheteshreyash <shreyashshete21@gmail.com>
2024-04-05 21:30:31 +0530

    deleted my copy

--- IE NGENEX/src/pages/Deals.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../style/App.css';
-
-export default function Deals() {
-  return <h1 className='deals'>SERVICES</h1>;
-}
--- IE NGENEX/src/pages/Products.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../style/App.css';
-
-export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
-}
